86735d5d826d1f4370c859e4ee7856aa64adfee0
baibol123 <baibolchik.002@gmail.com>
2023-12-17 21:21:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework_m3_31/Contact.java
@@ -0,0 +1,19 @@
+package com.example.homework_m3_31;
+
+public class Contact {
+    private final String name;
+    private final String surname;
+
+    public Contact(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
